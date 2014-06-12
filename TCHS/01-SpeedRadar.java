ce3554144166daf98ff929fc461103b86a856c03
chaos-git <ulterior.chaos@gmail.com>
2014-06-11 20:38:53 -0700

    Adding TCHS remaining 01 questions

---
+++ TCHS/01-SpeedRadar.java
@@ -0,0 +1,25 @@
+public class SpeedRadar {
+
+	public double averageSpeed(int minLimit, int maxLimit, int[] readings) {
+	
+		int totalInLimit = 0;
+		int totalSpeeds = 0;
+		
+		for (int i = 0; i < readings.length; i++) {
+		
+			int speed = readings[i];
+			if (speed >= minLimit && speed <= maxLimit) {
+				totalInLimit++;
+				totalSpeeds += speed;
+			}
+		
+		}
+		
+		if ((double)totalInLimit / readings.length < 0.9) {
+			return 0;
+		}
+		
+		return (double)totalSpeeds / totalInLimit;	
+	}
+	
+}
