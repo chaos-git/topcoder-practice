38a54b0d0bf984b490e41eaae1af2726f27a1f3f
chaos-git <ulterior.chaos@gmail.com>
2014-06-13 00:12:02 -0700

    Removing tabs BinaryCode.java

--- SRM/144-BinaryCode.java
+++ SRM/144-BinaryCode.java
@@ -1,33 +1,33 @@
 public class BinaryCode {
 
-	public String[] decode(String message) {
-		return new String[] { decode('0', message), decode('1', message) };
-	}
-	
-	public String decode(char firstValue, String encoded) {
-		char[] decoded = new char[encoded.length()];
-		decoded[0] = firstValue;
-		
-		int decPrev, decNext;
-		int decCur = Character.getNumericValue(firstValue);
+    public String[] decode(String message) {
+        return new String[] { decode('0', message), decode('1', message) };
+    }
+    
+    public String decode(char firstValue, String encoded) {
+        char[] decoded = new char[encoded.length()];
+        decoded[0] = firstValue;
+        
+        int decPrev, decNext;
+        int decCur = Character.getNumericValue(firstValue);
 
-		for (int i = 0; i < encoded.length(); i++) {
-			int encCur = Character.getNumericValue(encoded.charAt(i));
-			decNext = encCur - decCur - decPrev;
-			
-			if (i == encoded.length() - 1) {
-				if (encCur != decCur + decPrev) {
-					return "NONE";
-				}
-			} else if (decNext > 1 || decNext < 0) {
-				return "NONE";
-			}
+        for (int i = 0; i < encoded.length(); i++) {
+            int encCur = Character.getNumericValue(encoded.charAt(i));
+            decNext = encCur - decCur - decPrev;
+            
+            if (i == encoded.length() - 1) {
+                if (encCur != decCur + decPrev) {
+                    return "NONE";
+                }
+            } else if (decNext > 1 || decNext < 0) {
+                return "NONE";
+            }
 
-			decoded[i + 1] = decNext;
-			decPrev = decCur;
-			decCur = decNext;
-		}
-		
-		return new String(decoded);
-	}
+            decoded[i + 1] = decNext;
+            decPrev = decCur;
+            decCur = decNext;
+        }
+        
+        return new String(decoded);
+    }
 }
